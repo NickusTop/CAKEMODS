c3a79a73b07e52e00a7af8bf1e2f7d9525319787
NickusTop <148118087+NickusTop@users.noreply.github.com>
2025-03-29 19:51:11 +0200

    Sd

--- main-cake.js
+++ main-cake.js
@@ -1,9 +1,12 @@
 const aChoose = document.getElementById("a-choose");
+const btnSign = document.querySelector(".button-sig");
 const divChoose = document.querySelector(".div-choose");
+const menuSign = document.querySelector(".menu-sign");
 const overflowChoose = document.querySelector(".overflow-ch");
 
 aChoose.addEventListener("click", aChooseClick);
 overflowChoose.addEventListener("click", overflowChooseClick);
+btnSign.addEventListener("click", btnSignClick);
 function aChooseClick() {
   divChoose.style.display = "block";
   overflowChoose.style.display = "block";
@@ -12,4 +15,10 @@ function aChooseClick() {
 function overflowChooseClick() {
   divChoose.style.display = "none";
   overflowChoose.style.display = "none";
+  menuSign.style.display = "none";
+}
+
+function btnSignClick() {
+  menuSign.style.display = "block";
+  overflowChoose.style.display = "block";
 }
